3e1d533d5a1390410857615a3baf6d7b05298634
Guilherme Antônio <127310615+guilhermeantonioo@users.noreply.github.com>
2024-02-05 22:15:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const estrelas = document.querySelectorAll('.estrelas')
+const contador = document.querySelector('#contador')
+const btn = document.querySelector('#btn')
+const msg = document.querySelector('#mensagem')
+const text = document.querySelector
+('#texto-de-avaliacao')
+const avaliacao = document.querySelector('.avaliacao')
+let avaliacaoAtual = 0;
+
+
+
+
+document.addEventListener('click' , function(e) {
+  let classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    estrelas.forEach(function(star){
+      star.classList.remove('ativo')
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'))
+    contador.textContent =+ e.target.getAttribute('data-avaliacao')
+  }
+})
+
+btn.addEventListener("click" , () =>{
+  msg.classList.remove('hide')
+  setTimeout(function(){
+    msg.classList.add('hide')
+  } , 1200)
+})
+
+btn.addEventListener("click" , () =>{
+  setTimeout(function(){
+    text.classList.add('hide')
+    btn.classList.add('hide')
+    avaliacao.classList.add('hide')
+  } , 1)
+})
